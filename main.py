c223922e7c2a5070a8cc5c7717af6ddca4f28bd6
Deimon <Deimon1mail@gmail.com>
2023-11-03 20:41:37 +0200

    version2

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
     secret_number, attempts = game_logic.initialize_game(lower_limit, upper_limit)
 
     layout = [
-        [sg.Text(f"Введіть число між {lower_limit} та {upper_limit}: ")],
+        [sg.Text("Виберіть рівень складності:")],
+        [sg.Radio('Легкий (1-10)', "RADIO1", default=True, key='-EASY-'), sg.Radio('Середній (1-50)', "RADIO1", key='-MEDIUM-'), sg.Radio('Складний (1-100)', "RADIO1", key='-HARD-')],
+        [sg.Text(f"Введіть число: ")],
         [sg.InputText(key='-INPUT-')],
         [sg.Button('ОК?'), sg.Button('Вийти')]
     ]
@@ -20,20 +22,30 @@ def main():
             break
 
         user_guess = values['-INPUT-']
+        
+        # Определение уровня сложности
+        if values['-EASY-']:
+            lower_limit, upper_limit = 1, 10
+        elif values['-MEDIUM-']:
+            lower_limit, upper_limit = 1, 50
+        elif values['-HARD-']:
+            lower_limit, upper_limit = 1, 100
 
-        try:
-            user_guess = int(user_guess)
-            attempts += 1
+        if event == 'ОК?':
+            try:
+                user_guess = int(user_guess)
+                attempts += 1
 
-            if user_guess < secret_number:
-                sg.popup("Число більше.")
-            elif user_guess > secret_number:
-                sg.popup("Число менше.")
-            else:
-                sg.popup(f"Виграв {secret_number} за {attempts} спроб!")
-                break
-        except ValueError:
-            sg.popup("-_- нормалье число напишіть.")
+                if user_guess < secret_number:
+                    sg.popup("Число більше.")
+                elif user_guess > secret_number:
+                    sg.popup("Число менше.")
+                else:
+                    sg.popup(f"Виграв {secret_number} за {attempts} спроб!")
+                    # Начать новую игру
+                    secret_number, attempts = game_logic.initialize_game(lower_limit, upper_limit)
+            except ValueError:
+                sg.popup("-_- нормалье число напишіть.")
 
     window.close()
 
